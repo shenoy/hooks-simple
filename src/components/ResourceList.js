71d18c2ddd96f8350ab349b814a62f7e9b174dcd
RAJESH <rajeshtshenoy@gmail.com>
2019-12-06 20:01:54 +0000

    useEffect function added

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class ResourceList extends React.Component {
-  state = { resources: [] };
+const ResourceList = ({ resource }) => {
+  const [resources, setResources] = useState([]);
 
 
-  async componentDidMount() {
 
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-    this.setState({ resources: response.data });
-  }
+  useEffect(
+
+    () => {
+
+      (async resource => {
+
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`
+        );
+
+        setResources(response.data);
+
+      })(resource)
+
+    }, [resource]
+  );
 
 
-  async componentDidUpdate(prevProps) {
-    if (prevProps.resource !== this.props.resource) {
+  return <div>{resources.length}</div>;
 
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-      this.setState({ resources: response.data });
-    }
-  }
-
-  render() {
-    return <div>{this.state.resources.length}</div>
-  }
 }
 
 export default ResourceList;
